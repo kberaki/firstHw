6096fa6ce6c36212c5d60342a5d92ed827d6ec22
kberaki <berakik@gmail.com>
2018-10-22 17:06:22 -0400

    new files

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+let firstName = prompt('What is your first name')
+console.log(firstName);
+ let lastName = prompt('Enter your last name')
+console.log(lastName)
+ let city = prompt('Enter correct city address')
+ console.log(city)
+
+let completeInfo = confirm('I want to register')
+console.log(completeInfo);
+alert('Thank you for signing up ' + firstName + ' ' + lastName + ' from ' + city)
+
+
+
+//Let firstName = prompt('What is your  first name');
+//console.log(Name)
+
+
